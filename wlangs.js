b447919bdd1fbe26fce663d41c318c5c63cbf862
Alex Luckjanov <alex@luckjanov.ru>
2012-06-19 23:00:15 +0400

    Add clear non-list langs feature

--- wlangs.js
+++ wlangs.js
@@ -1,12 +1,21 @@
+var block = document.getElementById("p-lang");
+var links = block.getElementsByTagName('a');
+var links_size = links.length;
+
 chrome.extension.sendRequest({command: "get_langs"}, function(response){
 	var langs = response.langs_on_page;
 	var langs_size = langs.length;
-	alert(langs[0]);
-	alert(langs[1]);
-	var block = document.getElementById('p-lang');
-	alert(block);
-	for(var j = 0; j < langs_size; j++)
+	for(var i = 0; i < links_size; i++)
 	{
-		
+		for(var j = 0; j < langs_size; j++)
+			if(links[i].lang != langs[j] && j+1 == langs_size)
+			{
+				links[i].parentNode.parentNode.removeChild(links[i].parentNode);
+				i--;
+			}
+			else if(links[i].lang == langs[j])
+			{
+				break;
+			}
 	}
 });
